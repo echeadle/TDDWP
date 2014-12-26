423484eaeac1a8a86395eea42c5d052c85cff24f
Edward Cheadle <echeadle@gmail.com>
2014-12-26 07:41:57 -0700

    Fixed functional_tests/tests. It had leftover lines in it.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,13 +40,7 @@ class NewVisiterTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in the list
         inputbox.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table('Buy peacock feathers')
-
-        # import time
-        # time.sleep(10) 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a  text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -55,12 +49,8 @@ class NewVisiterTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
  
         # The page updates again, and now shows both items on her list.
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        #self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])        
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         # Edith wonders whether the site will remember her list. Then she
         # sees that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
